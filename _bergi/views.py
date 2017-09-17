f16dd3394c08147de5f3d84313f187189f21662e
duck2 <sikerbela@riseup.net>
2017-09-18 00:53:46 +0300

    minor fixes
    fix river-post margin after global box-sizing: content-box.
    shoehorn river-post into author page

--- _bergi/views.py
+++ _bergi/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
-
 from .models import *
 
 import random
@@ -19,7 +18,9 @@ def index(request):
 	return render(request, "index.html", ctx)
 
 def author(request, slug):
-	ctx = {"author": get_object_or_404(Author, slug=slug)}
+	author = get_object_or_404(Author, slug=slug)
+	ctx = {"author": author,
+		"author_articles": author.article_set.order_by("-date")}
 	return render(request, "author.html", ctx)
 
 # current recommendation algorithm is ducktape and close to random:
