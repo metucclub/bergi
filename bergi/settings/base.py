515f57c9d9ee2f8c4b6d1bea93c94bfad296ad62
Ozan Akın <ozn.akn@gmail.com>
2021-05-08 02:13:10 +0300

    update django

--- bergi/settings/base.py
+++ bergi/settings/base.py
@@ -123,6 +123,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
